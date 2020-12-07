a58252a3dec9f2650b47310bdf6b77885465db59
swolepenguin <ashtonm223@gmail.com>
2020-12-06 23:03:36 -0500

    getting it to display

--- routes/controllers/rawg.js
+++ routes/controllers/rawg.js
@@ -7,18 +7,19 @@ const ejslayouts = require('express-ejs-layouts')
 
 
 router.get('/', function(req,res){
-    // const url =`https://api.rawg.io/api/games?key=${process.env.API_KEY}&dates=2020-11-01,2020-12-01&platforms=18,1,7`
-    const url =`https://api.rawg.io/api/games?key=${process.env.API_KEY}&search='cyberpunk'`
+    const url =`https://api.rawg.io/api/games?key=${process.env.API_KEY}&dates=2020-11-01,2020-12-01&platforms=18,1,7`
+    // const url =`https://api.rawg.io/api/games?key=${process.env.API_KEY}&search='cyberpunk'`
     axios.get(url)
+    // .then(res => res.json(res.data.results))
     .then(data =>{ 
+        // console.log(data.data.results)
         console.log(data.data.results)
-        console.log('hello1')
-        res.send(data.data.results)
+        res.render('main' , {games : data.data.results})
     })
     // console.log('hello')
     // .then(res => {
     //     console.log(res.data)
-    //     res.render('main',{gData : res.data})
+    //     res.render('main',{gData : res.data.results})
     // })
     // console.log('hello')
     .catch(error => res.json(error))
